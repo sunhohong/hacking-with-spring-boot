81d58afe003119d870079399de022a38d568d058
Sunho Hong <anyuneed@gmail.com>
2022-11-28 18:11:45 +0900

    Chapther 1 - Add controller, service and model

---
+++ src/main/java/com/artieyoe/tutorials/springboot/hackingch1/models/Dish.java
@@ -0,0 +1,37 @@
+package com.artieyoe.tutorials.springboot.hackingch1.models;
+
+public class Dish {
+
+    private String description;
+    private boolean delivered = false;
+
+    public static Dish deliver(Dish dish) {
+        Dish deliveredDish = new Dish(dish.getDescription());
+        deliveredDish.delivered = true;
+        return deliveredDish;
+    }
+
+    public Dish(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDelivered() {
+        return delivered;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "description='" + description + '\'' +
+                ", delivered=" + delivered +
+                '}';
+    }
+}
